87acb4a97fcd66cdcf3389dd8b9b2b133c75d8ab
maomao2 <tanxiazhe@163.com>
2021-08-29 16:23:49 +0800

    init project with thymeleaf integration to spring boot2

---
+++ src/test/java/com/maomao2/spring/security/practise/ControlsysOauth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maomao2.spring.security.practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlsysOauth2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
